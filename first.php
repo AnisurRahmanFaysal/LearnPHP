0247f6c16d9cbc61ea22f7d91a657efc78fb2fd8
FaySal <anisur35-2601@diu.edu.bd>
2022-05-30 17:24:35 +0600

    Insert PHP code into HTML code

--- first.php
+++ first.php
@@ -1,4 +1,12 @@
-<?php
-echo "Hello Everyone"
-
- ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    echo "Hello"
+     ?>
+  </body>
+</html>
