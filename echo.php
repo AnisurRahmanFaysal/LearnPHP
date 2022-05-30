5a1813b57462cce17c3fd62e13d039ae6c9ec7cf
FaySal <anisur35-2601@diu.edu.bd>
2022-05-30 17:38:33 +0600

    Added echo and print

---
+++ echo.php
@@ -0,0 +1,19 @@
+<?php
+
+echo"Yahoo Baba";
+echo "Yahoo","Baba";
+echo "Yahoo"."Baba";
+echo ("Yahoo Baba");
+echo "<b>Yahoo Baba</b>";
+echo 5;
+
+print"Yahoo Baba";
+echo "Yahoo","Baba";
+print "Yahoo"."Baba";
+print ("Yahoo Baba");
+print "<b>Yahoo Baba</b>";
+print 5;
+
+
+
+?>
