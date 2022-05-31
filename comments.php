db2476893a776982c0400381c4205d11abac296d
FaySal <anisur35-2601@diu.edu.bd>
2022-05-31 21:57:29 +0600

    Added comments.php files and edit

---
+++ comments.php
@@ -0,0 +1,8 @@
+<?php
+
+//$x = "Yahoo Baba";  //This is called single line comment.
+echo $x . "<br>";   #This is also single line comment.
+var_dump($x);       /*but This is called multi line comments
+                    which is use of many lines.*/
+
+ ?>
