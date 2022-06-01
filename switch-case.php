fb38bf7032881bec9f3efd20fb9138d617a29a66
FaySal <anisur35-2601@diu.edu.bd>
2022-06-01 19:56:19 +0600

    Added weekenday in sweetch case

---
+++ switch-case.php
@@ -0,0 +1,30 @@
+<?php
+
+$weekday = 2;
+
+switch ($weekday) {
+  case 1:
+    echo "Today is sat";
+  break;
+  case 2:
+    echo "Today is sat";
+  break;
+  case 3:
+    echo "Today is sat";
+  break;
+  case 4:
+    echo "Today is sat";
+  break;
+  case 5:
+    echo "Today is sat";
+  break;
+  case 6:
+    echo "Today is sat";
+  break;
+    case 7:
+  echo "Today is sat";
+  break;
+
+}
+
+ ?>
