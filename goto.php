84020ce60b5adc0719fd17186267a1d41c559dce
FaySal <anisur35-2601@diu.edu.bd>
2022-06-02 02:42:04 +0600

    Added goto statement

---
+++ goto.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Hello Before goto<br>";
+echo "Hello Before goto<br>";
+goto abc;
+echo "Hello Before goto<br>";
+echo "Hello Before goto<br>";
+echo "Hello Before goto<br>";
+
+
+abc:
+
+echo "Hello,After Goto!";
+ ?>
