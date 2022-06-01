e8214581904dd801495121db68475452331c6d2c
FaySal <anisur35-2601@diu.edu.bd>
2022-06-01 18:37:52 +0600

    Added a result shit using if else statement

--- comparison.php
+++ comparison.php
@@ -1,11 +1,28 @@
 <?php
 
- $a = 25;
+ $per = 55;
 
- if($a == 25){
-   echo "They are equal";
+ if($per >= 80 && $per <= 100){
+   echo "You are in marrit";
  }
-else{
-  echo "It's not equal";
+elseif ($per >= 70 && $per < 80) {
+  echo "You are in secound position";
 }
+elseif ($per >= 60 && $per < 70) {
+  echo "You are in third position";
+}
+elseif ($per >= 50 && $per < 60) {
+  echo "Your are in four position";
+}
+elseif ($per >= 40 && $per < 50) {
+  echo "You are in five position";
+}
+elseif ($per >= 30 && $per < 40) {
+  echo "You are in six position";
+}
+elseif ($per >= 0 && $per < 30) {
+  echo "You are fail";
+}
+
+
  ?>
