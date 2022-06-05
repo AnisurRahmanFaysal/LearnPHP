ed1ea883a12b2538f8e072306275334afee385f8
FaySal <anisur35-2601@diu.edu.bd>
2022-06-05 23:10:39 +0600

    Added different datatype in a array

--- array.php
+++ array.php
@@ -1,7 +1,9 @@
 <?php
 
-$colors = array("red","blue","green");
+$colors = array("10","blue","100.25");
 
-echo $colors[2];
+echo $colors[0]."<br>";
+echo $colors[1]."<br>";
+echo $colors[2]."<br>";
 
  ?>
