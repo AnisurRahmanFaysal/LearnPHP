7fea561a25f052c74b74c9f39f343326cc9c45e9
FaySal <anisur35-2601@diu.edu.bd>
2022-06-05 23:13:44 +0600

    Added normal array and pre tag and print_r function

--- array.php
+++ array.php
@@ -1,9 +1,13 @@
 <?php
 
-$colors = array("10","blue","100.25");
+$colors = ["10","blue","100.25"];
 
 echo $colors[0]."<br>";
 echo $colors[1]."<br>";
 echo $colors[2]."<br>";
+
+echo "<pre>";
+print_r($colors);
+echo "</pre>";
 
  ?>
