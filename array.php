23c2a98834d0bc37f223404ad5ecc482457f0b8a
FaySal <anisur35-2601@diu.edu.bd>
2022-06-06 09:10:17 +0600

    Added counter for

--- array.php
+++ array.php
@@ -1,14 +1,22 @@
 <?php
 
-//$food = ["Orange","Banana","Apple","Grapes"];
+$food = ["Orange","Banana","Apple","Grapes"];
 
-$food = [
+/*$food = [
   "fruits" => ["Orange","Banana","Apple"],
   "Vegitable" => ["Carrot","Collard","Pea"]
 
-];
+];*/
 
-echo count($food['fruits'],1);
-echo sizeof($food,1);
+$len = count($food);
+//echo sizeof($food,1);
+
+for ($i=0; $i < $len ; $i++) {
+  echo $food[$i] . "<br>";
+}
+
+echo "<pre>";
+print_r(array_count_values($food));
+echo "</pre>";
 
  ?>
