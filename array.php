3f006c8aa9f3dc80dc67df9838a1ba31c3dda630
FaySal <anisur35-2601@diu.edu.bd>
2022-06-06 09:02:32 +0600

    Use of count and sizeof

--- array.php
+++ array.php
@@ -1,43 +1,14 @@
 <?php
 
-/*$emp = [
-  [1,"Faysal","Manager",500000],
-  [2,"Nishat","Frontend",500000],
-  [3,"Arimunnaher","Backend",800000],
-  [4,"Shoshi","Tester",200000],
-];*/
+//$food = ["Orange","Banana","Apple","Grapes"];
 
-$emp = [
-  ["ID"=> 1,
-  "Name"=>"Faysal",
-  "Designation"=>"Manager",
-  "Salary"=>500000],
-  ["ID"=>2,
-  "Name"=>"Nishat",
-  "Designation"=>"Frontend",
-  "Salary"=>500000],
-  ["ID"=>3,
-  "Name"=>"Arimunnaher",
-  "Designation"=>"Backend",
-  "Salary"=>800000],
-  ["ID"=>4,
-  "Name"=>"Shoshi",
-  "Designation"=>"Tester",
-  "Salary"=>200000],
+$food = [
+  "fruits" => ["Orange","Banana","Apple"],
+  "Vegitable" => ["Carrot","Collard","Pea"]
+
 ];
 
-echo "<table border='2px' cellpadding='3px' cellspacing='0'>
-
-  <tr>
-    <th>Emp ID</th>
-    <th>Name</th>
-    <th>Designation</th>
-    <th>Salary</th>
-  </tr>";
-
-foreach ($emp as list("ID" => $id,"Name" => $name,"Designation" =>$designtion,"Salary" =>$salary)) {
-  echo "<tr> <td>$id</td> <td>$name</td> <td>$designtion</td> <td>$salary </td> </tr>";
-}
-echo "</table>";
+echo count($food['fruits'],1);
+echo sizeof($food,1);
 
  ?>
