7f6927c321f5a28673d7a63ce1c1849ab833482a
FaySal <anisur35-2601@diu.edu.bd>
2022-06-06 09:22:44 +0600

    Added in array function

--- array.php
+++ array.php
@@ -2,21 +2,6 @@
 
 $food = ["Orange","Banana","Apple","Grapes"];
 
-/*$food = [
-  "fruits" => ["Orange","Banana","Apple"],
-  "Vegitable" => ["Carrot","Collard","Pea"]
-
-];*/
-
-$len = count($food);
-//echo sizeof($food,1);
-
-for ($i=0; $i < $len ; $i++) {
-  echo $food[$i] . "<br>";
-}
-
-echo "<pre>";
-print_r(array_count_values($food));
-echo "</pre>";
+echo in_array('Apple', $food);
 
  ?>
