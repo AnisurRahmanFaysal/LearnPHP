a4ee7620520bfa6ecaa873e43cc58e6a047e94cd
FaySal <anisur35-2601@diu.edu.bd>
2022-06-06 07:57:45 +0600

    Print multidamintional index array

--- array.php
+++ array.php
@@ -12,6 +12,23 @@ for ($row=0; $row < 3; $row++) {
   }
   echo "<br>";
 }
+echo "<table border='1px' cellpadding='5px' cellspacing='0'>";
+
+echo "<tr>
+      <th>Emp ID.</th>
+      <th>Name</th>
+      <th>Degination</th>
+      <th>Salary</th>
+    </tr>";
+
+foreach ($emp as $value) {
+  echo "<tr>";
+  foreach ($value as $value1) {
+    echo "<td> $value1 </td>";
+  }
+  echo "</tr>";
+}
+echo "</table>";
 
 echo "<pre>";
 print_r($emp);
