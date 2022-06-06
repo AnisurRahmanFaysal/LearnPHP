9f8a5479b820ddf70722d83478d853c748c4aa29
FaySal <anisur35-2601@diu.edu.bd>
2022-06-06 08:48:58 +0600

    Print associative array under foreach loop with list function

--- array.php
+++ array.php
@@ -1,10 +1,29 @@
 <?php
 
-$emp = [
+/*$emp = [
   [1,"Faysal","Manager",500000],
   [2,"Nishat","Frontend",500000],
   [3,"Arimunnaher","Backend",800000],
   [4,"Shoshi","Tester",200000],
+];*/
+
+$emp = [
+  ["ID"=> 1,
+  "Name"=>"Faysal",
+  "Designation"=>"Manager",
+  "Salary"=>500000],
+  ["ID"=>2,
+  "Name"=>"Nishat",
+  "Designation"=>"Frontend",
+  "Salary"=>500000],
+  ["ID"=>3,
+  "Name"=>"Arimunnaher",
+  "Designation"=>"Backend",
+  "Salary"=>800000],
+  ["ID"=>4,
+  "Name"=>"Shoshi",
+  "Designation"=>"Tester",
+  "Salary"=>200000],
 ];
 
 echo "<table border='2px' cellpadding='3px' cellspacing='0'>
@@ -16,7 +35,7 @@ echo "<table border='2px' cellpadding='3px' cellspacing='0'>
     <th>Salary</th>
   </tr>";
 
-foreach ($emp as list($id,$name,$designtion,$salary)) {
+foreach ($emp as list("ID" => $id,"Name" => $name,"Designation" =>$designtion,"Salary" =>$salary)) {
   echo "<tr> <td>$id</td> <td>$name</td> <td>$designtion</td> <td>$salary </td> </tr>";
 }
 echo "</table>";
