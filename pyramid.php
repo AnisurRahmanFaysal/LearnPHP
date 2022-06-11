34e25edb9125394236570992ee0c1cc970dd5965
FaySal <anisur35-2601@diu.edu.bd>
2022-06-11 23:28:06 +0600

    modified

--- pyramid.php
+++ pyramid.php
@@ -1,6 +1,6 @@
 <?php
 
-$n = (int)readline('Enter number:');
+$n = (int)readline();
 for ($i=1; $i < $n; $i++) { 
     for ($j=1; $j <=(2*$n)-1 ; $j++) { 
         if ( $j >= $n-($i-1) && $j <= $n+($i-1)) {
