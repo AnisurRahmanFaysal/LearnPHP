49a91f953a41211fe58c25d3d9e0a1f236111067
FaySal <anisur35-2601@diu.edu.bd>
2022-06-12 16:24:52 +0600

    Added fobinacci Series

---
+++ fibonacciSeries.php
@@ -0,0 +1,15 @@
+<?php
+
+    $number = (int)readline("Enter the number:");
+    $n0 = 0;
+    $n1 = 1;
+    echo $n0."\n";
+
+    for ($i=2; $i < $number; $i++) { 
+        $n = $n0 + $n1;
+        echo "$n\n";
+
+        $n0 = $n1;
+        $n1 = $n;
+    }
+?>
