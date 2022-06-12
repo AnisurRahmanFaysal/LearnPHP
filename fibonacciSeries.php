d08cf1aa66d0bb84a506f28a4cf0e37d875d74c4
FaySal <anisur35-2601@diu.edu.bd>
2022-06-12 20:01:52 +0600

    fixed fibonacciSeries

--- fibonacciSeries.php
+++ fibonacciSeries.php
@@ -3,7 +3,7 @@
     $number = (int)readline("Enter the number:");
     $n0 = 0;
     $n1 = 1;
-    echo $n0."\n";
+    echo $n0."\n".$n1."\n";
 
     for ($i=2; $i < $number; $i++) { 
         $n = $n0 + $n1;
