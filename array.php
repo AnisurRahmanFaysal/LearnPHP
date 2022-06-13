4f305e3279ec3dc9b6d91d936ddd2e5dfc3c20f7
FaySal <anisur35-2601@diu.edu.bd>
2022-06-13 16:17:41 +0600

    Combine 2 array

--- array.php
+++ array.php
@@ -1,35 +1,8 @@
 <?php
 
-//for ($i=0; $i < 5; $i++) { 
-    //$array[$i] = (int)readline("Enter ///$i index array values :");
-//}
-
-//var_dump($array);
-//echo(max($array[0],$array[1],$array//[2],$array[3],$array[4],))." is max value ///in this array\n";
-
-//foreach ($array as $key => $value) {
-  //  echo "$key => $value \n";
-//}
-
-//sort($array);
-
-//foreach ($array as $value) {
-  //  echo $value."\n";
-   
-//}
-
-
-
-$fruit = ['a' => "orange",'b' => "banana",'c' => "grapes"];
-
-$fruits = ['b' => ['color' => ['red','blue','green']],
-          'e' => "mango"];
-
-//$mergeArray = array_merge($fruit,$fruits);
-
-//$mergeArray= $fruit + $fruits;
-
-$mergeArray = array_merge_recursive($fruit,$fruits);
+$name = ["Faysal","Shoshi"];
+$age = ["25","23"];
+$mergeArray = array_combine($name,$age);
 
 echo "<pre>";
 print_r($mergeArray);
