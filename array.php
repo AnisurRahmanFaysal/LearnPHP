6cee48266f58191ba8d1ccfadea2773cc38a85f9
FaySal <anisur35-2601@diu.edu.bd>
2022-06-13 06:19:05 +0600

    Added array_unshift function

--- array.php
+++ array.php
@@ -1,21 +1,30 @@
 <?php
 
-for ($i=0; $i < 5; $i++) { 
-    $array[$i] = (int)readline("Enter $i index array values :");
-}
+//for ($i=0; $i < 5; $i++) { 
+    //$array[$i] = (int)readline("Enter ///$i index array values :");
+//}
 
 //var_dump($array);
-echo(max($array[0],$array[1],$array[2],$array[3],$array[4],))." is max value in this array\n";
+//echo(max($array[0],$array[1],$array//[2],$array[3],$array[4],))." is max value ///in this array\n";
 
-foreach ($array as $key => $value) {
-    echo "$key => $value \n";
-}
+//foreach ($array as $key => $value) {
+  //  echo "$key => $value \n";
+//}
 
-sort($array);
+//sort($array);
 
-foreach ($array as $value) {
-    echo $value."\n";
+//foreach ($array as $value) {
+  //  echo $value."\n";
    
-}
+//}
 
+
+
+$fruit = ["orange","banana","grapes"];
+
+array_unshift($fruit,"apple");
+
+echo "<pre>";
+print_r($fruit);
+echo"</pre>";
  ?>
