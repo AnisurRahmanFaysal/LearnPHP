cd62079bdae05febb3e870d226ab6950826bfa60
FaySal <anisur35-2601@diu.edu.bd>
2022-07-07 21:30:48 +0600

    Added array_keys function

--- array.php
+++ array.php
@@ -1,10 +1,11 @@
 <?php
 
-$color = ["red","green","blue",];
+$color = ["first"=>"red","second"=>"green","third"=>"blue",];
 
 $newArray = array_keys($color);
 
 echo "<pre>";
 print_r($newArray);
 echo"</pre>";
+
  ?>
